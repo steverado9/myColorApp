ba87b7dbd8cc21a28c03a58967e1755483b24a3a
stephen <isaacsteverado9@gmail.com>
2024-10-24 01:03:40 +0100

    modified app.js

--- app.js
+++ app.js
@@ -1,34 +1,44 @@
+const div = document.querySelector('#wrapper'); //get the parent element
+
 //A function to generate numbers from 1-100 and style them into even, odd, primeNumbers
 function generateNumbers() {
-    const div = document.querySelector('#wrapper'); //get the parent element
 
     for (let number = 1; number <= 100; number++) { // generating the numbers using loop
-       //Creating and appending elements to hold the numbers
+        //Creating and appending elements to hold the numbers
         const paragraph = document.createElement('p'); //creating a p tag element
         paragraph.textContent = number; //adding the numbers as text content
         div.appendChild(paragraph); // appending the p tag element to the parent div
-        // function for odd and even numbers
-       function evenOddNumbers () {
-            if (number % 2 === 0) {
-                paragraph.style.backgroundColor = "green"; //background style for even number
-            } else {
-                paragraph.style.backgroundColor = "yellow"; //background style for odd number
-            }
-       }
-       evenOddNumbers(); //calling the function for even and odd number
-       
-       //function for prime number
-       function primeNumber () {
-            if (number < 2) //if number is less than 2, it is not a prime number
-                return number; 
-            for (let i = 2; i < number; i++) {  //loop to keep dividing the number to check for multiples of the number
-                if (number % i == 0) { //if the number is divisible by i, it is not a prime number
-                    return number;
-                }
-            }
+
+        const evenOrOdd = isEvenOrOddNumber(number); //calling the function for even and odd number
+        if (evenOrOdd) {
+            paragraph.style.backgroundColor = "green"; //background style for even number
+        } else {
+            paragraph.style.backgroundColor = "yellow"; //background style for odd number
+        }
+
+        const isPrime = isPrimeNumber(number); //calling the function for prime number
+        if (isPrime) {
             paragraph.style.backgroundColor = "red"; //if the number gets here, it is a prime number
-       } 
-       primeNumber(); //calling the function for prime number
+        }
     }
 }
+
 generateNumbers(); //calling the function to generate number
+
+// function for odd and even numben % 2 === 0rs
+function isEvenOrOddNumber(n) {
+    return n % 2 === 0;
+}
+
+//function for prime number
+function isPrimeNumber(n) {
+    if (n < 2) //if number is less than 2, it is not a prime number
+        return false;
+
+    for (let i = 2; i < n; i++) {  //loop to keep dividing the number to check for multiples of the number
+        if (n % i == 0) { //if the number is divisible by i, it is not a prime number
+            return false;
+        }
+    }
+    return true;
+}
